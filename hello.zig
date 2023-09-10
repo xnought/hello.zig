1a840f56a96a8bd97d90a6ccb10f1e28f3d175a3
xnought <bertuccd@oregonstate.edu>
2023-09-09 19:37:40 -0700

    stip

--- hello.zig
+++ hello.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const print = std.debug.print;
+const allocator = std.heap.page_allocator;
 
 fn matrix(comptime t: type) type {
     return struct {
@@ -51,62 +52,15 @@ fn matrix(comptime t: type) type {
     };
 }
 
-const MatrixU8 = matrix(u8);
+const Matrix = matrix(f16);
 
-const Matrix = struct {
-    shape: [2]usize,
-    data: [*]f16,
-    pub fn init(data: [*]f16, shape: [2]usize) Matrix {
-        return Matrix{ .data = data, .shape = shape };
-    }
-    pub fn print(self: Matrix) void {
-        var m: usize = self.shape[0];
-        var n: usize = self.shape[1];
-        var i: usize = 0;
-        while (i < m) : (i += 1) {
-            var j: usize = 0;
-            std.debug.print("| ", .{});
-            while (j < n) : (j += 1) {
-                std.debug.print("{} ", .{self.value(i, j)});
-            }
-            std.debug.print("|\n", .{});
-        }
-    }
-    fn index(self: Matrix, i: usize, j: usize) usize {
-        return i * self.shape[1] + j;
-    }
-    fn value(self: Matrix, row: usize, col: usize) f16 {
-        return self.data[self.index(row, col)]; // self.data[i][j]
-    }
-    fn set_value(self: Matrix, row: usize, col: usize, new_value: f16) void {
-        self.data[self.index(row, col)] = new_value; // self.data[i][j] = value
-    }
-    pub fn matmul(self: Matrix, other: Matrix, out: Matrix) void {
-        var m: usize = self.shape[0];
-        var inner: usize = self.shape[1];
-        var n: usize = other.shape[1];
-
-        var i: usize = 0;
-        while (i < m) : (i += 1) {
-            var j: usize = 0;
-            while (j < n) : (j += 1) {
-                var k: usize = 0;
-                var summed: f16 = 0.0;
-                while (k < inner) : (k += 1) {
-                    summed += self.value(i, k) * other.value(k, j);
-                }
-                out.set_value(i, j, summed);
-            }
-        }
-    }
-};
-
-pub fn main() void {
-    var d: [4]u8 = .{ 1, 2, 3, 4 };
-    var out: [4]u8 = .{ 0, 0, 0, 0 };
+pub fn main() !void {
+    var d: [4]f16 = .{ 0, 1, 2, 3 };
+    var d_ptr: [*]f16 = &d;
+    var out: [4]f16 = .{ 0, 0, 0, 0 };
     var s = [2]usize{ 2, 2 };
-    var a = MatrixU8.init(&d, s);
-    var c = MatrixU8.init(&out, s);
+    var a = Matrix.init(d_ptr, s);
+    var c = Matrix.init(&out, s);
     a.matmul(a, c);
     c.print();
 }
